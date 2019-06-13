cbff44b9bf8e94340521e76ca5942e200b77b5da
juliaziqingcai <juliaziqingcai@gmail.com>
2019-06-13 09:38:21 -0400

    Deleting unecessary files. Don't mind the c++ file that is in progress

---
+++ JULIACOPY/Currently_Active_Code/NewEstimateN.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+//using namespace std; DON'T DO THIS IT IS BAD PRACTICE
+
+
+int main(){
+//
+    std::array<bool, N> A;
+    
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# NSERC-USRA-2019
-Code files for the summer 2019 USRA
